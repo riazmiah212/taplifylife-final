6ebf7be6eefb9c475dc4dc21806c785502a0b09c
riazmiah212 <riazmiah786212@gmail.com>
2025-06-29 16:09:38 +0100

    Delete app:[subdomain]:page .tsx

--- app:[subdomain]:page .tsx
+++
@@ -1,8 +0,0 @@
-export default function SubdomainPage({ params }: { params: { subdomain: string } }) {
-  return (
-    <main style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Welcome to {params.subdomain}.taplifylife.vercel.app!</h1>
-      <p>This is your live subdomain page.</p>
-    </main>
-  )
-}
