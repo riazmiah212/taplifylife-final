344af4e976ef2c367c7af17e41a530a9cc5e45b9
riazmiah212 <riazmiah786212@gmail.com>
2025-06-29 16:17:51 +0100

    Create page.tsx

---
+++ app/success/page.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+  return (
+    <main style={{ padding: '4rem', textAlign: 'center' }}>
+      <h1>Success!</h1>
+      <p>Your payment was successful. Your subdomain is being created.</p>
+    </main>
+  )
+}
