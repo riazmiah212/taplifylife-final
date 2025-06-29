293857dfdb4b7728f9cccf7121cf11a6c89d2711
riazmiah212 <riazmiah786212@gmail.com>
2025-06-29 16:09:29 +0100

    Delete app:cancel:page.tsx

--- app:cancel:page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CancelPage() {
-  return (
-    <main style={{ padding: '4rem', textAlign: 'center' }}>
-      <h1>Cancelled</h1>
-      <p>Your payment was cancelled. You can try again at any time.</p>
-    </main>
-  )
-}
