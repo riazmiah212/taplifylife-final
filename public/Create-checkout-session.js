8d83147968db761af2cf31e06a8061f8815951ed
riazmiah212 <riazmiah786212@gmail.com>
2025-06-29 15:56:49 +0100

    Delete public/Create-checkout-session.js

--- public/Create-checkout-session.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  // This would normally initiate a Stripe Checkout session
-  // For now, it's a placeholder redirect to the thank-you page
-  res.status(200).json({ url: '/thank-you.html' });
-}
